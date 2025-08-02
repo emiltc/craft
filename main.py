ebe8cd0aa514c7cae9fbb65b6592674c4fb97e02
emiltc <136669892+emiltc@users.noreply.github.com>
2025-08-02 13:06:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #
+import random
+
+projects = ["Project A", "Project B", "Project C"]
+weights = [0.1, 0.7, 0.2]  # Ưu tiên Project B
+print("Dự án ngẫu nhiên (có trọng số):", random.choices(projects, weights=weights, k=1)[0])
