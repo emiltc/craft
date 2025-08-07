e82345287ce492f54ff988b4d7cab3c05e10f987
emiltc <136669892+emiltc@users.noreply.github.com>
2025-08-07 10:04:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ projects = ["Project A", "Project B", "Project C"]
 weights = [0.1, 0.7, 0.2]  # Ưu tiên Project B
 print("Dự án ngẫu nhiên (có trọng số):", random.choices(projects, weights=weights, k=1)[0])
 
---02--03--05
+--02--03--05--07
